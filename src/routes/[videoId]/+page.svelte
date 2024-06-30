f0bbb5bdb051a92b5fdcf353a52283693bfdfde5
Aimen Altaiyeb <ymnghost@gmail.com>
2024-06-30 06:49:56 -0700

    Video Page

---
+++ src/routes/[videoId]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    let videoId = '';
+</script>
+
+<!--
+<iframe
+        class="w-full h-[480px]"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+/>
+-->
+
+<div class="max-w-[1150px] px-2 w-full mx-auto">
+    <h1 class="text-xl font-semibold capitalize">
+        svelte rules!!!
+    </h1>
+    <div class="flex flex-col md:flex-row gap-5 items-start" >
+        <div>
+
+        </div>
+    </div>
+</div>
